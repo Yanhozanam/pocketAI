b6107a11c3de48191fc3b9e72a4b211bbbbbf905
Yanhozanam <yanhozanam@gmail.com>
2026-06-30 15:39:34 +0200

    fix: handle existing git remote in llama.cpp clone step

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,4 @@
-﻿import 'dart:io';
-
+﻿
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
@@ -67,3 +66,4 @@ void main() {
     expect(find.byIcon(Icons.auto_awesome), findsOneWidget);
   });
 }
+
